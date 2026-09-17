93327da384444ca6bbc5f5576e91d0c4966fe07d
huynhtrunghao5629-dotcom <huynhtrunghao5629@gmail.com>
2026-09-17 14:55:47 +0700

    Tuan 1 /Bai 7/Kiem tra so nguyen to

---
+++ Bai7.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THUCHANH01
+{
+    public class Bai7
+    {
+        public bool kiemtrasnt(int n)
+        {
+            bool laSNT = true;
+
+            // Kiểm tra logic số nguyên tố
+            if (n < 2)
+            {
+                laSNT = false;
+            }
+            else
+            {
+                for (int i = 2; i <= Math.Sqrt(n); i++)
+                {
+                    if (n % i == 0)
+                    {
+                        laSNT = false;
+                        break;
+                    }
+                }
+            }
+
+            if (laSNT == true)
+            {
+                Console.WriteLine("Dung");
+            }
+            else
+            {
+                Console.WriteLine("Sai");
+            }
+
+            return laSNT;
+        }
+    }
+}
