747d25e0a62c862b8aa20f3f7ff029e63008b0c5
huynhtrunghao5629-dotcom <huynhtrunghao5629@gmail.com>
2026-09-17 15:00:24 +0700

    Tuan 1/Bai 9/Tham chieu out tim max min

---
+++ Bai9.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THUCHANH01
+{
+    public class Bai9
+    {
+        public void timminmax(double a, double b, double c, out double min, out double max)
+        {
+            // Dùng từ khóa out để có thể trả về nhiều kết quả qua tham số
+            min = Math.Min(a, Math.Min(b, c));
+            max = Math.Max(a, Math.Max(b, c));
+        }
+    }
+}
