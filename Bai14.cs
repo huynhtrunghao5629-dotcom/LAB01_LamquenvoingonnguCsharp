b2355b9d7f3315d2f0294ade67d5f077787aa9d8
huynhtrunghao5629-dotcom <huynhtrunghao5629@gmail.com>
2026-09-17 21:14:35 +0700

    Tuan 1/Bai 14/Tinh luong nhan vien

--- Bai14.cs
+++ Bai14.cs
@@ -6,27 +6,40 @@ using System.Threading.Tasks;
 
 namespace THUCHANH01
 {
-    public class Bai10
+    public class Bai14
     {
-        public bool kiemTraDoiXung(string s)
+        public string HoTen { get; set; }
+        public double MucLuong { get; set; }
+        public int SoNgayVang { get; set; }
+
+        public void Nhap()
         {
-            // Tự đảo ngược chuỗi ngay trong hàm này
-            char[] mangKyTu = s.ToCharArray();
-            Array.Reverse(mangKyTu);
-            string chuoiDao = new string(mangKyTu);
+            
+            Console.Write("Nhap ho ten nhan vien: ");
+            HoTen = Console.ReadLine();
 
-            // So sánh 2 chuỗi, bỏ qua việc phân biệt chữ hoa hay chữ thường
-            bool laDoiXung = s.Equals(chuoiDao, StringComparison.OrdinalIgnoreCase);
+            Console.Write("Nhap muc luong co ban : ");
+            MucLuong = double.Parse(Console.ReadLine());
 
-            if (laDoiXung == true)
-            {
-                Console.WriteLine("dung");
-            }
-            else
-            {
-                Console.WriteLine("khong");
-            }
-            return laDoiXung;
+            Console.Write("Nhap so ngay vang: ");
+            SoNgayVang = int.Parse(Console.ReadLine());
+        }
+
+        public double TinhLuong()
+        {
+            // Công thức tính lương thực nhận
+            return MucLuong - (SoNgayVang * 100000);
+        }
+
+        public void XuatLuong()
+        {
+            
+            Console.WriteLine($"\n- Ho ten: {HoTen}");
+            // Sử dụng :N0 để tự động phân cách hàng nghìn 
+            Console.WriteLine($"Muc luong goc: {MucLuong:N0} VND");
+            Console.WriteLine($"So ngay vang: {SoNgayVang} ngay");
+            Console.WriteLine($"Tien bi tru: {SoNgayVang * 100000:N0} VND");
+            Console.WriteLine($"Luong thuc nhan: {TinhLuong():N0} VND");
         }
     }
 }
