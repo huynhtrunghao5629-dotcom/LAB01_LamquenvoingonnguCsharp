2e457e8810d26f7a759120e99399614d3c5e71ef
huynhtrunghao5629-dotcom <huynhtrunghao5629@gmail.com>
2026-09-17 21:10:56 +0700

    Tuan 1/Bai 6/Phuong thuc tim gia tri lon nhat

--- Bai6.cs
+++ Bai6.cs
@@ -13,5 +13,21 @@ namespace THUCHANH01
             // Lồng 2 hàm Math.Max để tìm số lớn nhất trong 3 số
             return Math.Max(a, Math.Max(b, c));
         }
+
+        public void chayBai6()
+        {
+           
+            Console.Write("Nhap so nguyên thu nhat : ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhap so nguyên thu hai : ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhap so nguyên thu ba : ");
+            int c = int.Parse(Console.ReadLine());
+
+            int ketQua = max(a, b, c);
+            Console.WriteLine($"So lon nhat trong 3 so {a}, {b}, {c} la: {ketQua}");
+        }
     }
 }
