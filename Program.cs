a2dd9d46965f0c286d25d5f705e1e5a910c4f020
huynhtrunghao5629-dotcom <huynhtrunghao5629@gmail.com>
2026-09-17 21:15:09 +0700

    Main chinh

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+namespace THUCHANH01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+            //Console.OutputEncoding = System.Text.Encoding.UTF8;
+            //Bai1 b1 = new Bai1();
+            //b1.chayBai1();
+
+
+            //Bai2 bai2 = new Bai2();
+            //bai2.Chaybai2();
+
+
+            //Bai3_4 bai3_4 = new Bai3_4();
+            //bai3_4.Chaybai3_4();
+
+
+            //Bai5 b5 = new Bai5();
+            //b5.Chaybai5();
+
+
+            //Bai6 b6 = new Bai6();
+            //b6.chayBai6();
+
+            //Bai7 b7 = new Bai7();
+            //b7.chayBai7();
+
+
+            //Bai8 b8 = new Bai8();
+            //b8.chayBai8();
+
+
+            //Bai9 b9 = new Bai9();
+            //b9.chayBai9();
+
+
+            //bai10 b10 = new bai10();
+            //b10.chaybai10();
+
+
+            //Bai11 b11 = new Bai11();
+            //b11.chayBai11();
+
+
+            //Bai12 b12 = new Bai12();
+            //b12.chayBai12();
+
+
+
+
+            //Bai 13
+            //SinhVien sv = new SinhVien();
+            //sv.Nhap();
+            //sv.Xuat();
+
+            ////Bai 14
+            //Bai14 nv = new Bai14();
+            //nv.Nhap();
+            //nv.XuatLuong();
+
+
+            ////Bai 15
+            //Bai15 bai15 = new Bai15();
+            //bai15.chayBai15();
+
+            ////Bai 16
+            //Bai16 bai16 = new Bai16();
+            //bai16.sapXephoten();
+
+
+            ////Bai 17
+            //Bai17 bai17 = new Bai17();
+            //bai17.sinhMangngaunhien();
+            //bai17.inMang();
+            //ArrayList chan, le;
+            //bai17.traVechanle(out chan, out le);
+
+            //Console.Write("Cac so chan trong mang la: ");
+            //foreach (int so in chan)
+            //{
+            //    Console.Write(so + " ");
+            //}
+            //Console.Write("\nCac so le trong mang la: ");
+            //foreach (int so in le)
+            //{
+            //    Console.Write(so + " ");
+            //}
+            //Console.WriteLine();
+
+
+            Console.ReadLine();
+        }
+    }
+}
