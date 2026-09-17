f8664975b510a569f15de98b608639b501ec9674
huynhtrunghao5629-dotcom <huynhtrunghao5629@gmail.com>
2026-09-17 08:44:14 +0700

    Bai 1/Tuan 1/ Nhap xuat thong tin ho va ten

---
+++ Bai1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace THUCHANH01
+{
+    public class Bai1
+    {
+        public void chayBai1()
+        {
+            // In ra màn hình dòng chữ yêu cầu nhập họ tên
+            Console.Write("Nhap ho ten cua ban  : ");
+            // Đọc dữ liệu người dùng nhập từ bàn phím và lưu vào biến hoten
+            string hoten = Console.ReadLine();
+            // In ra kết quả kèm theo tên vừa nhập bằng phép cộng chuỗi
+            Console.WriteLine("Ho ten la : " + hoten);
+        }
+    }
+}
