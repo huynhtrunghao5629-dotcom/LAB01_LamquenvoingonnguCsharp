9f1863065637b44cf6ce93e04f6f0ddc1e1bc262
huynhtrunghao5629-dotcom <huynhtrunghao5629@gmail.com>
2026-09-17 15:01:43 +0700

    Tuan 1/Bai 10/Kiem tra chuoi doi xung

---
+++ Bai10.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THUCHANH01
+{
+    public class bai10
+    {
+        public bool kiemTraDoiXung(string s)
+        {
+            char[] mangKyTu = s.ToCharArray();
+            Array.Reverse(mangKyTu);
+            string chuoiDao = new string(mangKyTu);
+
+            bool laDoiXung = s.Equals(chuoiDao, StringComparison.OrdinalIgnoreCase);
+
+            if (laDoiXung == true)
+            {
+                Console.WriteLine("dung");
+            }
+            else
+            {
+                Console.WriteLine("khong");
+            }
+            return laDoiXung;
+        }
+    }
+}
