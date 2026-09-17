c7fc175256a3b13c9e430bf774686df18de98ef4
huynhtrunghao5629-dotcom <huynhtrunghao5629@gmail.com>
2026-09-17 14:58:54 +0700

    Tuan 1/Bai 8/Tham chieu ref hoan vi hai so

---
+++ Bai8.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THUCHANH01
+{
+    public class Bai8
+    {
+        public void hoanvi(ref double a, ref double b)
+        {
+            // Dùng từ khóa ref để can thiệp và thay đổi trực tiếp giá trị biến truyền vào
+            double temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
