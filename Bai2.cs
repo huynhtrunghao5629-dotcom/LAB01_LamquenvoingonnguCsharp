d16c85122d4b38683626c5ae4771b2f385463a74
huynhtrunghao5629-dotcom <huynhtrunghao5629@gmail.com>
2026-09-17 08:57:53 +0700

    Tuan 1 /Bai 2/Xuat va nhap chuoi

---
+++ Bai2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THUCHANH01
+{
+    public class Bai2
+    {
+        public void Chaybai2()
+        {
+            // Yêu cầu người dùng nhập họ tên
+            Console.Write("Nhap ho ten cua ban : ");
+            // Lưu dữ liệu nhập vào vào biến hoten
+            string hoten = Console.ReadLine();
+            // In ra câu chào, dùng $ để đưa trực tiếp biến hoten vào trong ngoặc {}
+            Console.WriteLine($"Chao ban {hoten} !");
+        }
+    }
+}
