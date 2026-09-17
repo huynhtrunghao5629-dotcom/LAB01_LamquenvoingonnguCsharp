cfe10e5aeba61fc2db4d1fe0cf4d8047f002de3c
huynhtrunghao5629-dotcom <huynhtrunghao5629@gmail.com>
2026-09-17 15:03:54 +0700

    Tuan 1/Bai 14/Tinh luong nhan vien

---
+++ Bai14.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THUCHANH01
+{
+    public class Bai10
+    {
+        public bool kiemTraDoiXung(string s)
+        {
+            // Tự đảo ngược chuỗi ngay trong hàm này
+            char[] mangKyTu = s.ToCharArray();
+            Array.Reverse(mangKyTu);
+            string chuoiDao = new string(mangKyTu);
+
+            // So sánh 2 chuỗi, bỏ qua việc phân biệt chữ hoa hay chữ thường
+            bool laDoiXung = s.Equals(chuoiDao, StringComparison.OrdinalIgnoreCase);
+
+            if (laDoiXung == true)
+            {
+                Console.WriteLine("dung");
+            }
+            else
+            {
+                Console.WriteLine("khong");
+            }
+            return laDoiXung;
+        }
+    }
+}
