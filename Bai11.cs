58678350f94111684577da6bec75dd1cd359ad75
huynhtrunghao5629-dotcom <huynhtrunghao5629@gmail.com>
2026-09-17 15:02:14 +0700

    Tuan 1/Bai 11/Dao nguoc chuoi

---
+++ Bai11.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THUCHANH01
+{
+    public class Bai11
+    {
+        public string traVeChuoiDao(string s)
+        {
+            // Chuyển chuỗi thành mảng các ký tự 
+            char[] mangKyTu = s.ToCharArray();
+            // Gọi hàm đảo ngược mảng
+            Array.Reverse(mangKyTu);
+            // Ghép mảng ký tự lại thành chuỗi mới
+            return new string(mangKyTu);
+        }
+    }
+}
