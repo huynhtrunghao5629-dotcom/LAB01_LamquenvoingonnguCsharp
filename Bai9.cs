f60e7c8d008cb87e77576870edadf2af7a3c10d1
huynhtrunghao5629-dotcom <huynhtrunghao5629@gmail.com>
2026-09-17 21:12:36 +0700

    Tuan 1/Bai 9/Tham chieu out tim max min

--- Bai9.cs
+++ Bai9.cs
@@ -10,9 +10,25 @@ namespace THUCHANH01
     {
         public void timminmax(double a, double b, double c, out double min, out double max)
         {
-            // Dùng từ khóa out để có thể trả về nhiều kết quả qua tham số
             min = Math.Min(a, Math.Min(b, c));
             max = Math.Max(a, Math.Max(b, c));
+        }
+
+        public void chayBai9()
+        {
+            Console.Write("Nhap so thuc thu nhat : ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Nhap so thuc thu hai : ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.Write("Nhap so thuc thu ba : ");
+            double c = double.Parse(Console.ReadLine());
+
+            double min, max;
+            timminmax(a, b, c, out min, out max);
+            Console.WriteLine($"Gia tri nho nhat la: {min}");
+            Console.WriteLine($"Gia tri lon nhat la: {max}");
         }
     }
 }
