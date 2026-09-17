d34bea6b0744796898ca8ff142851befa567e167
huynhtrunghao5629-dotcom <huynhtrunghao5629@gmail.com>
2026-09-17 15:02:46 +0700

    Tuan 1/Bai 12/Chuyen doi va dem tu trong chuoi

---
+++ Bai12.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THUCHANH01
+{
+    public class Bai12
+    {
+        public void xuLyChuoi(string s)
+        {
+            // In ra chuỗi viết thường và viết hoa
+            Console.WriteLine($"Chuoi chu thuong : {s.ToLower()}");
+            Console.WriteLine($"Chuoi chu hoa : {s.ToUpper()}");
+
+            // Cắt chuỗi dựa trên khoảng trắng, tab, và xuống dòng; tự động xóa các phần tử rỗng
+            string[] cacTu = s.Split(new char[] { ' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            // Độ dài của mảng sau khi cắt là số từ trong chuỗi
+            Console.WriteLine($"So tu trong chuoi : {cacTu.Length}");
+        }
+    }
+}
