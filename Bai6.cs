44706e7b549c11982247557af58313588cf0a1b2
huynhtrunghao5629-dotcom <huynhtrunghao5629@gmail.com>
2026-09-17 14:47:30 +0700

    Tuan 1/Bai 6/Phuong thuc tim gia tri lon nhat

---
+++ Bai6.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THUCHANH01
+{
+    public class Bai6
+    {
+        public int max(int a, int b, int c)
+        {
+            // Lồng 2 hàm Math.Max để tìm số lớn nhất trong 3 số
+            return Math.Max(a, Math.Max(b, c));
+        }
+    }
+}
